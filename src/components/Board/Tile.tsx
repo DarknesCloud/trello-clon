06b795198a75d3d62041ae7ab74235d7f9821a6c
Saul Leiva <aldairleiva42@gmail.com>
2022-10-06 14:55:13 -0600

    Feat Trello-clon

--- src/components/Board/Tile.tsx
+++ src/components/Board/Tile.tsx
@@ -6,7 +6,7 @@ export const Tile: React.FC<ITileProps> = ({ children }) => {
   return (
     <div
       className="bg-white py-2 px-2 mb-2 
-            transition-shadow shadow-lg rounded"
+           transition-shadow shadow-lg rounded"
     >
       <p className="text-sm">{children}</p>
     </div>
