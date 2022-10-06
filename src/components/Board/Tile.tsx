23b15bfc87ae3db9b13d195fdf37d30300f90bc0
Saul Leiva <aldairleiva42@gmail.com>
2022-10-06 11:57:31 -0600

    Feat Trello-clon

---
+++ src/components/Board/Tile.tsx
@@ -0,0 +1,14 @@
+interface ITileProps {
+  children: React.ReactNode;
+}
+
+export const Tile: React.FC<ITileProps> = ({ children }) => {
+  return (
+    <div
+      className="bg-white py-2 px-2 mb-2 
+            transition-shadow shadow-lg rounded"
+    >
+      <p className="text-sm">{children}</p>
+    </div>
+  );
+};
