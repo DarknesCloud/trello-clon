55989cbde897b67dcef6127928ffbfc72c988206
al3xdiaz <alexleonel96@hotmail.com>
2022-10-08 11:11:28 -0600

    :sparkles: Opacity y validacion
    Validacion para que no se guarden cards vacias y opacity 0.5 a cuando una card esta siendo seleccionada y arrastrada

--- src/components/Board/Tile.tsx
+++ src/components/Board/Tile.tsx
@@ -1,14 +1,14 @@
 interface ITileProps {
-  children: React.ReactNode;
+  children: React.ReactNode
+  opacity: string | undefined
 }
 
-export const Tile: React.FC<ITileProps> = ({ children }) => {
+export const Tile: React.FC<ITileProps> = ({ children, opacity }) => {
   return (
     <div
-      className="bg-white py-2 px-2 mb-2 
-           transition-shadow shadow-lg rounded"
+      className={`bg-white py-2 px-2 mb-2 transition-shadow shadow-lg rounded ${opacity}`}
     >
       <p className="text-sm">{children}</p>
     </div>
-  );
-};
+  )
+}
